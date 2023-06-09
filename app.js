3852676425a2d2843cc79d1f7cf75f83f8cb9727
Daniel <henio09@wp.pl>
2023-06-09 21:04:20 +0200

    get method works with mongo

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-const { todos } = require('./todos.cjs')
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { MongoClient } = require('mongodb');
 
-app.get('/todos', (req, res) => {
-  const userId = req.query.userId;
-  
-  const usersTodos = todos.filter(task => task.userId === Number(userId));
-  console.log(usersTodos);
-  res.send(usersTodos);
-})
+const app = express();
+const port = 3000;
 
-app.post('/todos', (req, res) => {
-  res.send('Got a POST request')
-})
+const dbName = 'todo';
+const mongoUrl = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(mongoUrl);
 
-app.put('/todos', (req, res) => {
-  res.send('Got a PUT request')
-})
 
-app.delete('/todos', (req, res) => {
-  res.send('Got a DELETE request')
-})
+app.get('/todos', async (req, res) => {
+  try {
+    const userId = req.query.userId;
+
+    const client = new MongoClient(mongoUrl);
+    await client.connect();
+    console.log('Database connection is ok!');
+
+    const db = client.db(dbName);
+
+    const todo = await db.collection('todos').find().toArray();;
+
+    if (todo) {
+      res.json(todo);
+    } else {
+      res.status(404).json({ error: 'Task not found' });
+    }
+  } catch (e) {
+    console.log('Error, cannot connect to the database:', e);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await client.close();
+    console.log('Database connection closed.');
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
 - created express.js server
 - used nodemon to automatically refresh the server, after every change
 - created simple endpoint /todos that handle GET by userId in query parameters
+- used mongo
+- get 
+- adapted the get method to mongo
+
+mongo start: bin\mongod --dbpath ./data
