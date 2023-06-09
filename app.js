56756e897f2d1d636df1e2b2071718736ae72338
Daniel <henio09@wp.pl>
2023-06-09 22:20:41 +0200

    post method is created

--- app.js
+++ app.js
@@ -8,24 +8,47 @@ const dbName = 'todo';
 const mongoUrl = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(mongoUrl);
 
-
 app.get('/todos', async (req, res) => {
   try {
     const userId = req.query.userId;
-
     const client = new MongoClient(mongoUrl);
+
     await client.connect();
-    console.log('Database connection is ok!');
 
     const db = client.db(dbName);
-
-    const todo = await db.collection('todos').find().toArray();;
+    const todo = await db.collection('todos').find().toArray();
 
     if (todo) {
       res.json(todo);
     } else {
       res.status(404).json({ error: 'Task not found' });
     }
+  } catch (e) {
+    console.log('Error, cannot connect to the database:', e);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await client.close();
+    console.log('Database connection closed.');
+  }
+});
+
+app.post('/todos', async (req, res) => {
+  try {
+    const {userId, title, completed} = req.query;
+    const client = new MongoClient(mongoUrl);
+
+    await client.connect();
+
+    const db = client.db(dbName);
+    const result = await db.collection('todos').insertOne({
+      "userId": userId,
+      "title": title,
+      "completed": completed
+    });
+    const insertedId = result.insertedId; // Pobierz pierwsze utworzone todo z result.ops
+    const insertedTodo = await db.collection('todos').findOne({ "_id": insertedId }); // Pobierz todo na podstawie identyfikatora
+
+    res.json(insertedTodo);
   } catch (e) {
     console.log('Error, cannot connect to the database:', e);
     res.status(500).json({ error: 'Internal server error' });
--- readme.md
+++ readme.md
@@ -4,7 +4,5 @@
 - used nodemon to automatically refresh the server, after every change
 - created simple endpoint /todos that handle GET by userId in query parameters
 - used mongo
-- get 
-- adapted the get method to mongo
 
 mongo start: bin\mongod --dbpath ./data
