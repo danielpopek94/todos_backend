86e9e23f957548d691341f27a992a596e301369d
Daniel <henio09@wp.pl>
2023-06-11 19:06:25 +0200

    cleaned up in server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = 3000;
-
-const dbName = 'todo';
-const mongoUrl = 'mongodb+srv://admin:admin@todoapp.bbycyl2.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(mongoUrl);
 
 app.use(cors());
 app.use(bodyParser.json());
